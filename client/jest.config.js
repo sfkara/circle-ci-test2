f4f0ddaf5e3afcf29c5ee08e2b50fda87e2d92ba
sfkara <seriffiratkara@gmail.com>
2021-10-21 16:01:22 +0300

    update jest 5

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
-  reporters: [
-    'default', [
-        'jest-junit',
-        {
-            outputDirectory: 'result',
-            outputName: 'frontend-result.xml',
-        },
-    ],
-],
+  preset: '@vue/cli-plugin-unit-jest'
+//   reporters: [
+//     'default', [
+//         'jest-junit',
+//         {
+//             outputDirectory: 'result',
+//             outputName: 'frontend-result.xml',
+//         },
+//     ],
+// ],
 
 }
